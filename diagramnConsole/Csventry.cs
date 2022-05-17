d8e64a476e9bea0bec0e707cdafb1f12f60488dc
mllll <mllll@DESKTOP-3U1DESO>
2022-05-17 13:59:39 +0100

    Add project files.

---
+++ diagramnConsole/Csventry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diagramnConsole
+{
+    public class Csventry
+    {
+        public int Count { get; set; }
+        public string Name { get; set; } = "";
+
+    }
+}
---
+++ diagramnConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using diagramnConsole;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+string destination = "show_file.txt";
+List<Csventry> csventry = new List<Csventry>();
+string[] lines = File.ReadAllLines(destination);
+
+var rx = new Regex(@"\s+",RegexOptions.Compiled);
+
+foreach (string line in lines)
+{
+    string[] columns = rx.Split(line);
+
+    if (columns.Length != 2)
+    {
+        continue;
+    }
+    csventry.Add(new Csventry{ Count = int.Parse(columns[1]), Name = columns[0] }) ;
+}
